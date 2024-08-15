42b36a7c900f26e35cb38e6d95449b954ad8e25c
Rihanss <rizkymahesli@gmail.com>
2024-08-15 14:14:18 +0700

    Added error catching. With webhook

--- src/commands/image/image.js
+++ src/commands/image/image.js
@@ -10,19 +10,19 @@ module.exports = class Image extends Command {
 		super(client, {
 			data: new SlashCommandBuilder()
 				.setName('image')
-				.setDescription('[Holo | Fun] Tell Holo to retrieve specified image.')
+				.setDescription('[Holo | Image] Tell Holo to retrieve specified image.')
 				.setDMPermission(true)
 				.addSubcommand(subcommand => subcommand
 					.setName('kemonomimi')
-					.setDescription('[Holo | Fun | Image] Get a kemonomimi image.'),
+					.setDescription('[Holo | Image] Get a kemonomimi image.'),
 				)
 				.addSubcommand(subcommand => subcommand
 					.setName('holo')
-					.setDescription('[Holo | Fun | Image] Get a holo image.'),
+					.setDescription('[Holo | Image] Get a holo image.'),
 				)
 				.addSubcommand(subcommand => subcommand
 					.setName('captcha')
-					.setDescription('[Holo | Fun | Image] Set someone as captcha!')
+					.setDescription('[Holo | Image] Set someone as captcha!')
 					.addUserOption(option => option
 						.setName('user')
 						.setDescription('The user you want to turn into captcha'),
@@ -30,11 +30,23 @@ module.exports = class Image extends Command {
 				)
 				.addSubcommand(subcommand => subcommand
 					.setName('changemymind')
-					.setDescription('[Holo | Fun | Image] Change my mind')
+					.setDescription('[Holo | Image] Change my mind')
 					.addStringOption(option => option
 						.setName('text')
 						.setDescription('Text for changemymind')
 						.setRequired(true),
+					),
+				)
+				.addSubcommand(subcommand => subcommand
+					.setName('whowouldwin')
+					.setDescription('[Holo | Image] Who would win between this and this?')
+					.addUserOption(option => option
+						.setName('user1')
+						.setDescription('First user'),
+					)
+					.addUserOption(option => option
+						.setName('user2')
+						.setDescription('Second user'),
 					),
 				),
 			usage: 'image <option>',
@@ -51,6 +63,9 @@ module.exports = class Image extends Command {
 
 		const user = interaction.options.getUser('user') || interaction.user;
 		const text = interaction.options.getString('text');
+
+		const user1 = interaction.options.getUser('user1') || interaction.user;
+		const user2 = interaction.options.getUser('user1') || interaction.user;
 		switch (subcommand) {
 		case 'kemonomimi': {
 			const { body } = await get('https://nekobot.xyz/api/image?type=kemonomimi');
@@ -88,6 +103,19 @@ module.exports = class Image extends Command {
 		}
 		case 'changemymind': {
 			const { body } = await get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${text}`);
+			const embed = new EmbedBuilder()
+				.setImage(body.message)
+				.setColor('Random')
+				.setTimestamp();
+
+			interaction.editReply({ embeds: [embed] });
+			break;
+		}
+		case 'whowouldwin:': {
+			const avatar1 = user1.avatarURL({ dynamic: false, extension: 'png' });
+			const avatar2 = user2.avatarURL({ dynamic: false, extension: 'png' });
+
+			const { body } = await get(`https://nekobot.xyz/api/imagegen?type=whowouldwin&user1=${avatar1}&user2=${avatar2}`);
 			const embed = new EmbedBuilder()
 				.setImage(body.message)
 				.setColor('Random')
