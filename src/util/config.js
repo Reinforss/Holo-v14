c4784b78b39c44917a9865d8560def27192dbf64
Rihanss <rizkymahesli@gmail.com>
2024-12-23 17:23:18 +0700

    update

--- src/util/config.js
+++ src/util/config.js
@@ -14,16 +14,11 @@ module.exports = {
 		reconnectTimeout: 10000,
 	},
 	restnode: {
-		// host: 'lava-v4.ajieblogs.eu.org',
-		// port: 443,
-		// password: 'https://dsc.gg/ajidevserver',
-		// secure: true,
 		host: process.env.MUSICHOST,
 		port: 2333,
 		password: process.env.MUSICPASSWORD,
 	},
 	nodes: [
-		// { name: process.env.NODE_NAME || 'Astera', host: 'lava-v4.ajieblogs.eu.org', port: 443, region: 'asia', password: 'https://dsc.gg/ajidevserver', secure: true },
 		{ name: process.env.NODE_NAME || 'Astera', host: process.env.MUSICHOST, port: 2333, region: 'asia', password: process.env.MUSICPASSWORD },
 	],
 };
